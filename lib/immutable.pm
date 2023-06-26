defc6c8167dbe6d7d8881592b12944ea4b381b72
Ingy döt Net <ingy@ingy.net>
2023-06-26 16:22:20 -0400

    CPAN Release 0.0.4
    - Add immutable::seq support

--- lib/immutable.pm
+++ lib/immutable.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package immutable;
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 sub import {
     die "Currently invalid to 'use immutable;'. Try 'use immutable::0;'.";
